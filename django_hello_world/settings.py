1cfd1e4615c3c7bedfd829c2c30bfcd876e5538a
yuri1984 <garmoncheg@gmail.com>
2013-11-15 19:13:33 +0200

    ticket:4 for #4 fixed for deployment admin support

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -95,6 +95,10 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django_hello_world.context_processors.django_settings',
 )
