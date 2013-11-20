eb3ee202de1057b16f26ed54a2bfa8f6ba87ec7a
yuri1984 <garmoncheg@gmail.com>
2013-11-20 18:01:15 +0200

    ticket:5 for #5 serving media and static files for debug

--- django_hello_world/urls.py
+++ django_hello_world/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -15,3 +17,11 @@ urlpatterns = patterns('',
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'})
 )
+
+if settings.DEBUG:
+    # Media for debug
+    urlpatterns += patterns('django.views.static',
+        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+    )
+    # Static for debug
+    urlpatterns += staticfiles_urlpatterns()
