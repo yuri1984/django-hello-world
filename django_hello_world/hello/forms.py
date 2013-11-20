5984b08bb55744e598e9c3128001e9dddb90b66f
yuri1984 <garmoncheg@gmail.com>
2013-11-20 20:07:43 +0200

    ticket:6 for #6 usage of Django Admin widget for edit home form

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
+
 from models import Owner
 
 
 class EditOwner(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=AdminDateWidget)
     class Meta:
         model = Owner
